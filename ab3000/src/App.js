f414ab1218f0814e8bb24e767547cc47efa43e0d
AB3000 <aarushban@gmail.com>
2019-11-04 20:23:37 -0500

    Created path depth function and added timeout

--- ab3000/src/App.js
+++ ab3000/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import HomePage from './pages/homePage';
 import ProfessionalPage from './pages/professionalPage'
 
+//transition group components for smooth transition to pages
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+
 class App extends Component {
 
   constructor(props) {
@@ -11,15 +14,30 @@ class App extends Component {
     this.state = {};
   }
 
+  //gets path's depth
+  getPathDepth(location){
+    // gives path e.g: home is '/', professional is '/professional'
+    let pathArr = location.pathname.split('/');
+    //returns all elements in path that are not empty strings
+    pathArr = pathArr.filter(n => n !== '');
+    return pathArr.length; 
+  }
+
   render() {
     const { location } = this.props;
+
+    //time it takes for mounting the components and applying the animation (ms)
+    const { timeout } = { enter: 800, exit: 400 };
     return (
-      <div className='App'>
-        <Switch location={location}>
-          <Route path = '/' exact component={HomePage} />
-          <Route path = '/professional' exact component={ProfessionalPage} />
-        </Switch>
-      </div>
+      <TransitionGroup component='div' className='App'>
+        {/* for smooth animation */}
+        <CSSTransition timeout={timeout} className='pageSlider' mountOnEnter={false} mountOnExit={true} >
+          <Switch location={location} >
+            <Route path='/' exact component={HomePage} />   {/* depth 0*/}
+            <Route path='/professional' exact component={ProfessionalPage} />  {/* depth 1*/}
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 }
