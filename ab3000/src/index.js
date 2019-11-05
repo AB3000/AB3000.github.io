0552ec12104c8bbe71f961177b66a6da926e4718
AB3000 <aarushban@gmail.com>
2019-11-05 17:59:14 -0500

    Removed extra comments

--- ab3000/src/index.js
+++ ab3000/src/index.js
@@ -27,12 +27,10 @@ const theme = createMuiTheme({
         // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
         borderRadius: 3,
         border: 0,
-        // color: "white",
         height: 70,
         padding: "0px 70px",
         margin: "100px",
         textDecoration: "none",
-        // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .30)"
       }
     }
   }
