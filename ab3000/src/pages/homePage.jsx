b865733faaf36dfbb5e847244942aeb71b408753
AB3000 <aarushban@gmail.com>
2019-11-05 01:52:46 -0500

    Dilineated hobbies and professional work with buttons

--- ab3000/src/pages/homePage.jsx
+++ ab3000/src/pages/homePage.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import './homeStyle.scss';
-import { Link } from 'react-router-dom';
-import posed, { PoseGroup } from 'react-pose';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import Text from "react-text";
+import "./homeStyle.scss";
+import { Link } from "react-router-dom";
+import SplitText from "react-pose-text";
+import Button from "@material-ui/core/Button";
+
+const charPoses = {
+  exit: { opacity: 0, y: 20 },
+  enter: {
+    opacity: 1,
+    y: 0,
+    delay: ({ charIndex }) => charIndex * 30
+  }
+};
+
 
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div className='page-container page'>
-        <div className='home-page inner-container'>
-          <div className='welcome-container'>
-            Welcome aboard! Glad to see you here!
-              <Link to='/professional'>
-                <Button>Hobbies</Button>
-              </Link>
-              <Link to='/professional'>
-                <Button>Professional</Button>
-              </Link>
-          </div>
-        </div>
+      <div className="home-container page">
+        <br></br>
+        <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+          Welcome aboard!
+        </SplitText>
+        <br></br>
+        <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+          My name is Aarushi.
+        </SplitText>
+        <br></br>
+        <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+          Glad to have you here!
+        </SplitText>
+        <br></br>
+        <Link to="/hobbies">
+          <Button color="primary" variant="contained">
+            Hobbies
+          </Button>
+        </Link>
+        <Link to="/professional">
+          <Button color="primary" variant="contained">
+            Professional
+          </Button>
+        </Link>
       </div>
     );
   }
