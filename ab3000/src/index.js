1e89d3ad279ba34ecbee2bb0b1ff8b2fe06b0fbd
AB3000 <aarushban@gmail.com>
2019-11-05 01:53:04 -0500

    Configured theme for materials-ui

--- ab3000/src/index.js
+++ ab3000/src/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#fff",
+      main: "rgb(23, 105, 170)",
+      dark: "#000"
+    },
+    secondary: {
+      main: "#f44336"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        // Name of the rule
+        // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+        borderRadius: 3,
+        border: 0,
+        // color: "white",
+        height: 70,
+        padding: "0px 70px",
+        margin: "100px",
+        textDecoration: "none",
+        // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .30)"
+      }
+    }
+  }
+});
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
