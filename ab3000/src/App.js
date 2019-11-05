9fd8a8d61565f0b15b9b61553b6bf51c4e1028d1
AB3000 <aarushban@gmail.com>
2019-11-04 19:54:32 -0500

    Added the routes and temporary tabs

--- ab3000/src/App.js
+++ ab3000/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/homePage';
+import ProfessionalPage from './pages/professionalPage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { location } = this.props;
+    return (
+      <div className='App'>
+        <Switch location={location}>
+          <Route path = '/' exact component={HomePage} />
+          <Route path = '/professional' exact component={ProfessionalPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
