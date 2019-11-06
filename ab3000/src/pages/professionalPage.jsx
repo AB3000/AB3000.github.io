bb1b58c3cd9d3a28adc62e6f5842c63905a733df
AB3000 <aarushban@gmail.com>
2019-11-05 20:49:48 -0500

    Added a timeline structure to work experience page

--- ab3000/src/pages/professionalPage.jsx
+++ ab3000/src/pages/professionalPage.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import { professionalData } from "../data/variables";
 
-export default class AboutPage extends React.Component {
+import "./professionalStyle.scss";
+
+//creates a timeline based on data from variables.js
+//Credits to Florin Pop for the tutorial!
+const TimelineItem = ({ data }) => (
+  <div className="timeline-item">
+    <div className="timeline-item-content">
+      <span className="tag" style={{ background: data.category.color }}>
+        {data.category.tag}
+      </span>
+      <time>{data.date}</time>
+      <p>{data.text}</p>
+      {data.link && (
+        <a href={data.link.url} target="_blank" rel="noopener noreferrer">
+          {data.link.text}
+        </a>
+      )}
+      {data.secondLink && (
+        <a href={data.secondLink.url} target="_blank" rel="noopener noreferrer">
+          {data.secondLink.text}
+        </a>
+      )}
+      <span className="circle" />
+    </div>
+  </div>
+);
+
+//check if at least one element in the array
+const Timeline = () =>
+  professionalData.length > 0 && (
+    <div className="timeline-container">
+      {professionalData.map((data, idx) => (
+        <TimelineItem data={data} key={idx} />
+      ))}
+    </div>
+  );
+
+export default class ProfessionalPage extends React.Component {
   render() {
     return (
-      <div className='page-container page'>
-      <div className='home-page inner-container'>
-        <div className='welcome-container'>
-          Welcome aboard! Glad to see you here!
-            <Link to='/'>
-              <button className='btn'>Translate to Next Page</button>
+      <div className="page-container page">
+        <div className="home-page inner-container">
+          <div className="welcome-container">
+            <Link to="/">
+              <Button>Home</Button>
             </Link>
+            <Link to="/hobbies">
+              <Button>Hobbies</Button>
+            </Link>
+          </div>
         </div>
+        <Timeline></Timeline>
       </div>
-    </div>
     );
   }
 }
