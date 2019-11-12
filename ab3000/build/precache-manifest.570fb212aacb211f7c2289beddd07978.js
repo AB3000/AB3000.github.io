ac78f9b8ade14146d0c13fc70a3b84051050e29f
AB3000 <aarushban@gmail.com>
2019-11-12 14:06:59 -0500

    delete all thisdsa

---
+++ ab3000/build/precache-manifest.570fb212aacb211f7c2289beddd07978.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c83f79a2609cb752a729699202e0abb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23f8f926a3ba063bbf53",
+    "url": "/static/css/main.1c7f5f5d.chunk.css"
+  },
+  {
+    "revision": "d4129fbdb54c4cb69043",
+    "url": "/static/js/2.5df1848d.chunk.js"
+  },
+  {
+    "revision": "23f8f926a3ba063bbf53",
+    "url": "/static/js/main.0c917774.chunk.js"
+  },
+  {
+    "revision": "f5189b733acf6b90cf85",
+    "url": "/static/js/runtime-main.eee5cf1d.js"
+  }
+]);
---
+++ ab3000/src/pages/hobbiesPage.jsx
@@ -0,0 +1,81 @@
+// import React from "react";
+// import "./homeStyle.scss";
+// import { Link } from "react-router-dom";
+// import { Button, Grid } from "@material-ui/core";
+// import { Row, Col } from "reactstrap";
+// import Swiper from "react-id-swiper";
+
+// import "swiper/css/swiper.css";
+
+// const params = {
+//   pagination: ".swiper-pagination",
+//   paginationClickable: true,
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflow: {
+//     rotate: 50,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true
+//   }
+// };
+
+// export default class HobbiesPage extends React.Component {
+//   render() {
+//     return (
+//       <div className="page-container page">
+//         <div className="home-page inner-container">
+//           <div className="welcome-container">
+//             <Link to="/">
+//               <Button>Home</Button>
+//             </Link>
+//             <Link to="/professional">
+//               <Button>Professional</Button>
+//             </Link>
+//           </div>
+//           <Row>
+//             <Col sm="12" md="12" lg="12">
+//               <Swiper {...params}>
+//                 <img
+//                   src={require("../assets/images/Web-Files/AnimationClips/abstract.png")}
+//                   alt="Smiley face"
+//                   height="50px"
+//                   width="20px"
+//                 ></img>
+//                 <img
+//                   src={require("../assets/images/Web-Files/AnimationClips/abstract.png")}
+//                   alt="Smiley face"
+//                   height="50px"
+//                   width="20px"
+//                 ></img>
+//                                 <img
+//                   src={require("../assets/images/Web-Files/AnimationClips/abstract.png")}
+//                   alt="Smiley face"
+//                   height="50px"
+//                   width="20px"
+//                 ></img>
+//               </Swiper>
+//             </Col>
+//           </Row>
+//           <Swiper {...params}>
+//             <div>
+//               <img
+//                 src={require("../assets/images/Web-Files/AnimationClips/abstract.png")}
+//                 alt="Smiley face"
+//               ></img>
+//             </div>
+//             <div>
+//               <img
+//                 src={require("../assets/images/Web-Files/AnimationClips/abstract.png")}
+//                 alt="Smiley face"
+//               ></img>
+//             </div>
+//           </Swiper>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
