9e4beba29f20c235e071ff5cada7e412d0b9bc96
AB3000 <aarushban@gmail.com>
2019-11-13 21:38:39 -0500

    Updates

---
+++ precache-manifest.9cc78df05700b8680a01dbfdbd3cc154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46730dba67ef5d6f197ad803161bc60f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fabc72a8e4b6a6d51017",
+    "url": "/static/css/main.8b33b75f.chunk.css"
+  },
+  {
+    "revision": "0efaf912beeb321858eb",
+    "url": "/static/js/2.04e32797.chunk.js"
+  },
+  {
+    "revision": "fabc72a8e4b6a6d51017",
+    "url": "/static/js/main.c5e6f86a.chunk.js"
+  },
+  {
+    "revision": "94fb6e2742216416fe45",
+    "url": "/static/js/runtime-main.14f4df82.js"
+  }
+]);
