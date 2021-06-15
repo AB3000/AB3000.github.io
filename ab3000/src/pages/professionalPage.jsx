73629def386b230f2623a1ce6eb46677edc8de50
AB3000 <aarushban@gmail.com>
2021-06-14 19:36:33 -0700

    removing deadcode

--- ab3000/src/pages/professionalPage.jsx
+++ ab3000/src/pages/professionalPage.jsx
@@ -12,46 +12,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
-//creates a timeline based on data from variables.js
-//Credits to Florin Pop for the tutorial!
-
-{
-  /* <VerticalTimelineElement
-className="vertical-timeline-element--education"
-date="November 2012"
-iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
-// icon={<SchoolIcon />}
->
-<h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-<h4 className="vertical-timeline-element-subtitle">Certification</h4>
-<p>
-  Creative Direction, User Experience, Visual Design
-</p>
-</VerticalTimelineElement> */
-}
 
 const TimelineItem = ({ data }) => (
   <VerticalTimelineElement
     date={data.date}
-    // className={data.className}
     contentStyle={{ background: "#ff8787", color: "#000" }}
     iconStyle={{ background: "#424242", color: data.iconColor }}
     icon={
       <FontAwesomeIcon transform={data.iconOffset} icon={data.icon} size="2x" />
     }
   >
-    {/* <div className="timeline-item-content"> */}
-    {/* <span className="tag" style={{ background: data.category.color }}>
-        {data.category.tag}
-      </span> */}
     {data.title && <h2>{data.title}</h2>}
     {data.position && <h4>Position: {data.position}</h4>}
-    {/* {data.location && <h5>Location: {data.location}</h5>} */}
     <time>{data.location}</time>
-    {/* {data.text} */}
     {"text" in data ? splitBullets(data.text) : ""}
     {"links" in data ? addLinks(data.links) : ""}
-    {/* </div> */}
   </VerticalTimelineElement>
 );
 
