264115c5f6657fa73160a41f15a2e8a27a95361a
AB3000 <aarushban@gmail.com>
2021-10-23 01:03:38 -0400

    icons working

--- ab3000/src/pages/jsx/professionalPage.jsx
+++ ab3000/src/pages/jsx/professionalPage.jsx
@@ -35,10 +35,10 @@ const TimelineItem = ({ data }) => (
     boxShadow: `0 3px 0 0px ${data.boxBorder}`}}
     iconStyle={{ background: "iconBackground" in data ? data.iconBackground : "#424248", color: data.iconColor, 
     boxShadow: "iconOutline" in data ? `0 0 0 4px ${data.iconOutline}` : "0 0 0 4px rgb(249, 205, 255)"}}
-    icon={
-        // <FontAwesomeIcon transform={data.iconOffset} icon={data.icon} size="2x"/>
-        <span class="icon-california" data-icon="&#xe900;" className="rainbow-gradient"
-        style={{}}></span>
+    icon={ "iconGradient" in data ? 
+        <span class="icon-california" data-icon="&#xe900;" className="rainbow-gradient" style={{fontSize: "1.5em"}}/> :
+        <FontAwesomeIcon transform={data.iconOffset} icon={data.icon} size="2x"/> 
+        
     }
     contentArrowStyle={{ borderRight: "7px solid  #424242" }}
   >
